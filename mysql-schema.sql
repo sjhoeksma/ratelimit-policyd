5b4e96286f6866637c95ef9c2ef9ad7b9cf23f6e
Philip Iezzi <github@onlime.ch>
2015-01-04 00:27:46 +0100

    set unique key on sender column

--- mysql-schema.sql
+++ mysql-schema.sql
@@ -14,11 +14,12 @@ USE `policyd`;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ratelimit` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`sender` VARCHAR(255) NOT NULL,
+	`sender` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL,
 	`quota` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	`used` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	`expiry` INT(10) UNSIGNED DEFAULT NULL,
-	PRIMARY KEY (`id`))
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `idx_sender` (`sender` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
